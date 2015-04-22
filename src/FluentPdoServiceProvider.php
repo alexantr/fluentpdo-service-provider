bb2830a923727aa79df3fcee26ed8fbc3b29ec39
Alexander <alex.yashkin@gmail.com>
2015-04-22 11:49:02 +0300

    simplify

--- src/FluentPdoServiceProvider.php
+++ src/FluentPdoServiceProvider.php
@@ -5,6 +5,11 @@ namespace Alexantr\Silex\Provider;
 use Pimple\Container;
 use Pimple\ServiceProviderInterface;
 
+/**
+ * FluentPDO Service Provider
+ *
+ * @author Alex Yashkin <alex.yashkin@gmail.com>
+ */
 class FluentPdoServiceProvider implements ServiceProviderInterface
 {
     public function register(Container $app)
@@ -18,12 +23,8 @@ class FluentPdoServiceProvider implements ServiceProviderInterface
         $app['fpdo.debug'] = false;
 
         $app['fpdo'] = function ($app) {
-            $options = $app['fpdo.pdo_options'];
-            $dsn = isset($options['dsn']) ? $options['dsn'] : 'mysql:dbname=test;host=localhost';
-            $username = isset($options['username']) ? $options['username'] : 'root';
-            $password = isset($options['password']) ? $options['password'] : '';
-            $options = isset($options['options']) && is_array($options['options']) ? $options['options'] : array();
-            $pdo = new \PDO($dsn, $username, $password, $options);
+            $opt = $app['fpdo.pdo_options'];
+            $pdo = new \PDO($opt['dsn'], $opt['username'], $opt['password'], (array)$opt['options']);
             $fpdo = new \FluentPDO($pdo);
             $fpdo->debug = $app['fpdo.debug'];
             return $fpdo;
